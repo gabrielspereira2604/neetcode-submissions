dc8224c69541b8a6f940b3ed3cd9debf45d061b0
gabrielspereira2604 <gabrielspereira2604@gmail.com>
2026-06-02 21:04:27 -0300

    125. Valid Palindrome

---
+++ Leetcode/Valid-Palindrome/submission.cs
@@ -0,0 +1,22 @@
+using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsPalindrome(string s) {
+        s = Regex.Replace(s, @"[^a-zA-Z0-9]", "").ToLower();
+
+        if(s.Length > 0){
+            int l = 0;
+            int r = s.Length - 1;
+            while(l < r){
+                if(s[l] != s[r]){
+                    return false;
+                }
+
+                l++;
+                r--;
+            }
+        }
+
+        return true;
+    }
+}
